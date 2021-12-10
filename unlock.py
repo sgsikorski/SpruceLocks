bb0dea15a8f60dd8cc626348b811cd4a0bbaea0f
Scott Sikorski <ssikorsk@u.rochester.edu>
2021-12-09 21:07:45 -0500

    Python to run on PI

--- unlock.py
+++ unlock.py
@@ -1,10 +1,48 @@
 import bs4
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import RPi.GPIO as gpio
 
 url = 'https://sgsikorski.github.io/SpruceLocks'
-
 doc = requests.get(url)
 
-print (bs4.BeautifulSoup(doc.text, "html.parser"))
+gpio.setup(17, gpio.out)
+gpio.setup(18, gpio.out)
+gpio.setup(19, gpio.out)
+gpio.setup(20, gpio.out)
+gpio.setup(21, gpio.out)
+gpio.setup(22, gpio.out)
+gpio.setup(23, gpio.out)
+gpio.setup(24, gpio.out)
+gpio.setup(25, gpio.out)
+gpio.setup(26, gpio.out)
+
+driver = webdriver.Chrome("C:\\Users\\Scott Sikorski\\CS.Workspace\\SideProjects\\chromedriver")
+driver.get(url)
+
+action = ActionChains(driver)
+ele = driver.find_element_by_id("status")
+action.click()
+
+def GPIOoutput(pin1, pin2, direction):
+    dir1 = True if direction == 'lock' else False
+    gpio.output(pin1, dir1)
+    gpio.output(pin2, False if dir1==True else True)
+
+for line in (bs4.BeautifulSoup(doc.text, "html.parser")):
+    if (line.contains('</p>')):
+        if (line.contains('u1')):
+            GPIOoutput(17,18,'unlock') if line.contains('Un') else GPIOoutput(17,18,'lock')
+        elif (line.contains('u2')):
+            GPIOoutput(19,20,'unlock') if line.contains('Un') else GPIOoutput(19,20,'lock')
+        elif (line.contains('u3')):
+            GPIOoutput(21,22,'unlock') if line.contains('Un') else GPIOoutput(21,22,'lock')
+        elif (line.contains('u4')):
+            GPIOoutput(23,24,'unlock') if line.contains('Un') else GPIOoutput(23,24,'lock')
+        elif (line.contains('u4')):
+            GPIOoutput(25,26,'unlock') if line.contains('Un') else GPIOoutput(25,26,'lock')
+
+
 
 
--- unlockRequests.js
+++ unlockRequests.js
@@ -1,35 +1,54 @@
-var R1 = true
-var R2 = true
-var R3 = true
-var R4 = true
-var R5 = true
+
+// var u2 = document.getElementById("u2").innerHTML
+// var u3 = document.getElementById("u3").innerHTML
+// var u4 = documnet.getElementById("u4").innerHTML
+// var u5 = document.getElementById("u5").innerHTML
+
+
+function onOpen(){
+    document.getElementById("u1").innerHTML = localStorage.getItem("status1");
+    document.getElementById("u2").innerHTML = localStorage.getItem("status2");
+    document.getElementById("u3").innerHTML = localStorage.getItem("status3");
+    document.getElementById("u4").innerHTML = localStorage.getItem("status4");
+    document.getElementById("u5").innerHTML = localStorage.getItem("status5");
+}
 
 function onR1(){
-    R1 = R1 == true ? false : true;
-    var ele = document.getElementById("u1").innerHTML
-    document.getElementById("u1").innerHTML = ele.includes("Un") ? "Locked" : "Unlocked";
+    var r1 = document.getElementById("u1").innerHTML
+    var flip = r1.includes("Un") ? "Locked" : "Unlocked";
+    
+    localStorage.setItem("status1", flip);
+    document.getElementById("u1").innerHTML = flip;
 }
 
 function onR2(){
-    R2 = R2 == true ? false : true;
-    var ele = document.getElementById("u2").innerHTML
-    document.getElementById("u2").innerHTML = ele.includes("Un") ? "Locked" : "Unlocked";
+    var r2 = document.getElementById("u2").innerHTML
+    var flip = r2.includes("Un") ? "Locked" : "Unlocked";
+    
+    localStorage.setItem("status2", flip);
+    document.getElementById("u2").innerHTML = flip;
 }
 
 function onR3(){
-    R3 = R3 == true ? false : true;
-    var ele = document.getElementById("u3").innerHTML
-    document.getElementById("u3").innerHTML = ele.includes("Un") ? "Locked" : "Unlocked";
+    var r3 = document.getElementById("u3").innerHTML
+    var flip = r3.includes("Un") ? "Locked" : "Unlocked";
+    
+    localStorage.setItem("status3", flip);
+    document.getElementById("u3").innerHTML = flip;
 }
 
 function onR4(){
-    R4 = R4 == true ? false : true;
-    var ele = document.getElementById("u4").innerHTML
-    document.getElementById("u4").innerHTML = ele.includes("Un") ? "Locked" : "Unlocked";
+    var r4 = document.getElementById("u4").innerHTML
+    var flip = r4.includes("Un") ? "Locked" : "Unlocked";
+    
+    localStorage.setItem("status4", flip);
+    document.getElementById("u4").innerHTML = flip;
 }
 
 function onR5(){
-    R5 = R5 == true ? false : true;
-    var ele = document.getElementById("u5").innerHTML
-    document.getElementById("u5").innerHTML = ele.includes("Un") ? "Locked" : "Unlocked";
+    var r5 = document.getElementById("u5").innerHTML
+    var flip = r5.includes("Un") ? "Locked" : "Unlocked";
+    
+    localStorage.setItem("status5", flip);
+    document.getElementById("u5").innerHTML = flip;
 }
